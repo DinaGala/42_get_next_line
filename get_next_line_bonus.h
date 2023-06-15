3802d40c780bd01a604daf77830ab2ebf567d806
Dina Zhuzhleva <106472944+DinaGala@users.noreply.github.com>
2023-06-15 20:25:06 +0200

    open final version

---
+++ get_next_line_bonus.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nzhuzhle <nzhuzhle@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/15 19:27:13 by nzhuzhle          #+#    #+#             */
+/*   Updated: 2023/06/15 19:28:18 by nzhuzhle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 24
+# endif
+
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+}	t_list;
+
+/**** get_next_line.c ****************************************/
+
+char	*get_next_line(int fd);
+void	read_to_buf(int fd, t_list **list, long *lenth);
+long	save_to_list(t_list **list, char *buf, long bytes);
+void	add_to_string(t_list *list, char *str, long lenth);
+int		clean_list(t_list **list);
+
+/**** get_next_line_utils.c *********************************/
+
+int		check_line(t_list *node, long *lenth);
+void	*free_all(t_list **list, char *str);
+t_list	*last_node(t_list *list);
+size_t	str_lenth(char *str);
+char	*rest_str(char *last);
+
+#endif
